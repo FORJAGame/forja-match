f1b9500df047569be74bd51377b5ca8a18395945
Erick Simões <erick.simoes@live.com>
2026-06-12 19:38:03 -0300

    Refactor StartScreen component layout and improve text consistency ane FORJA logo

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,16 +1,18 @@
+import forja_logo from '../assets/forja_match_logo.png'
+
 function StartScreen({ onStart }) {
   return (
     <section className="screen start-screen">
-      <div className="brand-mark">FORJA</div>
-
-      <h1>FORJA Match</h1>
+      <header className='title'>
+        <img id='forja-logo' src={forja_logo} alt="FORJA Match Logo"/>
+      </header>
 
       <p className="subtitle">
-        Descubra qual jogo da FORJA combina mais com você.
+        Descubra qual jogo da FORJA combina mais com você
       </p>
 
       <p className="instructions">
-        Leia as frases e escolha se elas têm ou não a ver com seu jeito de jogar.
+        Leia as frases e escolha se elas têm ou não a ver com seu jeito de jogar
       </p>
 
       <button className="primary-button" onClick={onStart}>
